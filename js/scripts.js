4385eac3efcefd826e1882c168db7c5964beba6b
joansaj <joannsaj@gmail.com>
2020-08-30 21:24:04 +0300

    add hovers

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,29 @@ $(document).ready(function () {
         $(".click3").toggle();
     });
 
+
     $("#img1").hover(function () {
         $(".hover1").toggle();
-    })
+    });
+    $("#img2").hover(function () {
+        $(".hover2").toggle();
+    });
+    $("#img3").hover(function () {
+        $(".hover3").toggle();
+    });
+    $("#img4").hover(function () {
+        $(".hover4").toggle();
+    });
+    $("#img5").hover(function () {
+        $(".hover5").toggle();
+    });
+    $("#img6").hover(function () {
+        $(".hover6").toggle();
+    });
+    $("#img7").hover(function () {
+        $(".hover7").toggle();
+    });
+    $("#img8").hover(function () {
+        $(".hover8").toggle();
+    });
 });
